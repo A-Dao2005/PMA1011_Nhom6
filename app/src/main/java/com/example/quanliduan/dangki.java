f7bde44bc63e73ee1c27c927cce975ad9313522e
Bùi văn việt <buiviet289@gmail.com>
2025-01-19 23:20:34 +0700

    viet

---
+++ app/src/main/java/com/example/quanliduan/dangki.java
@@ -0,0 +1,57 @@
+package com.example.quanliduan;
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quanliduan.database.QuanLyNguoiDungSqlite;
+
+public class dangki extends AppCompatActivity {
+
+    private EditText etUsername, etPassword, etConfirmPassword;
+    private Button btnRegister;
+    private QuanLyNguoiDungSqlite dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangki);  // Layout đăng ký của bạn
+
+        etUsername = findViewById(R.id.edtUsernameRegister);
+        etPassword = findViewById(R.id.edtPasswordRegister);
+        etConfirmPassword = findViewById(R.id.et_confirm_password);
+        btnRegister = findViewById(R.id.btnRegister);
+        dbHelper = new QuanLyNguoiDungSqlite(this);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String confirmPassword = etConfirmPassword.getText().toString();
+
+                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast.makeText(dangki.this, "Vui lòng điền đầy đủ thông tin!", Toast.LENGTH_SHORT).show();
+                }else if (!password.equals(confirmPassword)) {
+                    Toast.makeText(dangki.this, "Mật khẩu không trùng khớp!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    boolean success = dbHelper.insertUser(username, password);
+                    if (success) {
+                        Toast.makeText(dangki.this, "Đăng Kí Thành Công!", Toast.LENGTH_SHORT).show();
+                        finish(); // Go back to login
+                    } else {
+                        Toast.makeText(dangki.this, "Tài khoản đã tồn tại", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+};
---
+++ app/src/main/java/com/example/quanliduan/dangnhap.java
@@ -0,0 +1,66 @@
+package com.example.quanliduan;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quanliduan.database.QuanLyNguoiDungSqlite;
+
+public class dangnhap extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private Button btnLogin, btnDangki;
+    private QuanLyNguoiDungSqlite dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dangnhap);
+
+        etUsername = findViewById(R.id.etUsernameLogin);
+        etPassword = findViewById(R.id.etPasswordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnDangki = findViewById(R.id.btnDangki);
+
+        dbHelper = new QuanLyNguoiDungSqlite(this);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(dangnhap.this, "Vui Lòng điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean isValid = dbHelper.checkUser(username, password);
+                    if (isValid) {
+                        Toast.makeText(dangnhap.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                        // Navigate to main activity
+                        Intent intent = new Intent(dangnhap.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(dangnhap.this, "Tên Đăng nhập hoặc mật khẩu sai!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        btnDangki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(dangnhap.this, dangki.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
