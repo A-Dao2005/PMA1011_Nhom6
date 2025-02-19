ddcda0b4bcdcae213d5d4d750d160981492b6d73
Bùi văn việt <buiviet289@gmail.com>
2025-02-19 20:50:03 +0700

    viet

---
+++ app/src/main/java/com/example/quanliduan/fragment/IncomeFragment.java
@@ -0,0 +1,64 @@
+package com.example.quanliduan.fragment;
+
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.quanliduan.R;
+import com.example.quanliduan.adapter.adapterthunhap;
+import com.example.quanliduan.adapter.adaterchitieu;
+
+
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.quanliduan.R;
+
+public class IncomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_income, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        // Dữ liệu mẫu
+        List<String> categoryList = new ArrayList<>();
+
+        categoryList.add("bán hàng");
+        categoryList.add("lương");
+        categoryList.add("ngân hàng");
+
+        // Gán Adapter vào RecyclerView
+        adapterthunhap adapter = new adapterthunhap(categoryList);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+}
+
